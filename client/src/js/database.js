175e8183763ada6ebeca2d724627f5860f9349ab
kate.sundquist2 <kate.sundquist2@gmail.com>
2023-06-11 23:06:46 -0700

    modified database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,21 +7,48 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', {
+         keyPath: 'id', 
+         autoIncrement: true
+      });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+  // if (putDbImplemented) {
+  //   putDb(content);
+  // }else{
   console.error('putDb not implemented');
+  // }
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', readonly);
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
 
-  console.error('getDb not implemented');
+    return result?.value;
+
+  // if (getDbImplemented) {
+  //   getDb(content)
+  // }else{
+  // console.error('getDb not implemented');
+  // } 
 };
 
 
